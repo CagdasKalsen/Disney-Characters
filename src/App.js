88fde10b7308c77fea7bcc2b1b4abe9c55719bc4
cagdaskalsen <cagdaskalsen@hotmail.com>
2022-12-07 12:03:11 -0500

    added some css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Pages from "./pages/Pages";
 // import Character from "./components/Characters";
 // import { Routes, Route } from "react-router-dom";
-// import "./App.css";
+import "./App.css";
 // import { Link } from "react-router-dom";
 
 function App() {
