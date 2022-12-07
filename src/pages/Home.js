96565b338132f4fa6c2e9ceea62f1a194cdbe6d3
Brandon Tobin <brandontobinbusiness@gmail.com>
2022-12-07 13:39:29 -0600

    going for most recent dev changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,22 +4,6 @@ import React from "react";
 import Characters from "../components/Characters";
 
 function Home() {
-  // const [character, setCharacter] = useState(null);
-
-  // async function fetchId() {
-  //   try {
-  //     const response = await fetch(`https://api.disneyapi.dev/characters`);
-  //     const characterData = await response.json();
-  //     setCharacter(characterData);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   fetchId();
-  // }, []);
-
   return (
     <div className="container">
       <div className="navbar">
