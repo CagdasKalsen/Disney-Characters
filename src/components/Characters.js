fac718e436b4959174a0f4d67672b1664fc99a31
Brandon Tobin <brandontobinbusiness@gmail.com>
2022-12-09 14:02:29 -0600

    Only need to work on Character Details (map)?

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -4,7 +4,6 @@ import { useParams } from "react-router";
 
 function Characters(props) {
   const [character, setCharacter] = useState(null);
-
   async function fetchCharacter() {
     try {
       const response = await fetch(`https://api.disneyapi.dev/characters`);
@@ -19,18 +18,21 @@ function Characters(props) {
   useEffect(() => {
     fetchCharacter();
   }, []);
+
   return (
     <div className="container">
+      
       {character &&
         character.map((char, index) => {
           return (
             <div className="card">
               <Link id="name" to={`/characters/${char._id}`} key={index}>
-                {/* <div className="name"> */}
+                
+                <div className="name">
                 <p className="charname">
                   Character <br /> {char.name}
                 </p>
-                {/* </div> */}
+                </div>
                 <div className="image">
                   <img src={char.imageUrl} alt="" />
                 </div>
@@ -41,4 +43,5 @@ function Characters(props) {
     </div>
   );
 }
+
 export default Characters;
